940b3789b4601adc184b38b369dc452c1e73b83e
Santiago Zaidan <santyagozaidan@gmail.com>
2023-02-15 23:49:10 -0300

    bugs

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/products', productsRoutes)
 app.use('/api/users', usersRoutes)
+app.use('/api/products', productsRoutes)
 
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Endpoint not found' })
